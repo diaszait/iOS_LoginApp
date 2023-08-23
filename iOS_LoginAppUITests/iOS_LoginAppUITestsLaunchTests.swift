7f724c3e0f101e6c4d6b8d5a5a9c9ca77d5e9239
diaszait <diasabub@gmail.com>
2023-08-23 18:25:03 +0600

    iOS_LoginApp

---
+++ iOS_LoginAppUITests/iOS_LoginAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_LoginAppUITestsLaunchTests.swift
+//  iOS_LoginAppUITests
+//
+//  Created by Dias Zait on 21.08.2023.
+//
+
+import XCTest
+
+final class iOS_LoginAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
